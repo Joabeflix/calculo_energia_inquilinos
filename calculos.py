a7b4961627248cf51610b81d05b0d6ff5705f512
joabeflix <joabealvesyt@gmail.com>
2026-01-12 13:29:42 -0300

    git commit -m "Initial commit"

---
+++ calculos.py
@@ -0,0 +1,16 @@
+import json
+
+
+class ValoresKwh:
+    def __init__(self) -> None:
+
+        self.dados_valores = ...
+    
+    @property
+    def preco_base(self) -> float:
+        ...
+
+
+
+
+
